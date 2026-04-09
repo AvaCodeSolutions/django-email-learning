7bf2493b3d767806f6ee50a88bfd1a13d64a8ed1
Payam <payam.nj@gmail.com>
2026-04-09 09:07:33 +0400

    Remove credit from the public pages

--- frontend/public/components/Layout.jsx
+++ frontend/public/components/Layout.jsx
@@ -9,44 +9,6 @@ const Layout = ({ children }) => {
         <Container sx={{ backgroundColor: 'background.paper', padding: 4, marginTop: { xs: 0, md: "32px" }, borderRadius: 2, boxShadow: 2 }}>
         {children}
         </Container>
-
-        {/* Footer Credit for Public Pages */}
-        <Box
-          component="footer"
-          sx={{
-            position: 'fixed',
-            bottom: 0,
-            right: 16,
-            padding: 1,
-            zIndex: 1000,
-            backgroundColor: 'transparent'
-          }}
-        >
-          <Typography
-            variant="caption"
-            sx={{
-              color: 'text.secondary',
-              fontSize: '0.7rem',
-              opacity: 0.8
-            }}
-          >
-            Powered by{' '}
-            <Link
-              href="https://github.com/AvaCodeSolutions/django-email-learning"
-              target="_blank"
-              rel="noopener noreferrer"
-              sx={{
-                color: 'primary.main',
-                textDecoration: 'none',
-                '&:hover': {
-                  textDecoration: 'underline'
-                }
-              }}
-            >
-              Django Email Learning
-            </Link>
-          </Typography>
-        </Box>
     </>);
 }
 
